a988d9c06f49f39b72dadf070c22c153b813fcda
Jacob Tyler <jacobtyler22@icloud.com>
2015-01-22 12:06:19 -0700

    Update

--- service.js
+++ service.js
@@ -1,12 +1,20 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
+	var deferred = $q.defer();
 
+    this.getUsers = function() {
+	    $http({
+	        method: 'GET',
+	        url: 'http://reqr.es/api/users?page=1'
+	    }).then(function(res){
+	    	res = res.data.data;
+	    	for(var i = 0; i < res.length; i++){
+	    		res[i].first_name = 'Ralf';
+	    	}
+	    	deferred.resolve(res);
+	    });
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
-  }
+	    return deferred.promise;
+    };
 });
